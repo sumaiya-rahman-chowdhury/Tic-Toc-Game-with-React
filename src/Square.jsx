d72513bc8ec85db199ad6c89a0143c8ae857fc63
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-09-08 11:40:30 -0700

    added X when you clicked

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,5 +1,36 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
 import './index.css'
 
-export default function Square(){
-    return <button className="square">X</button>
+export default function Square() {
+    function Board() {
+        const [value, setValue] = useState(null)
+
+        function onClickHandlers() {
+            setValue('X')
+        }
+        return <button className="font-bold border-[1px]  border-black  p-10" onClick={onClickHandlers}>{value}</button>
+    }
+    return <>
+        <div className='flex justify-center items-center  h-screen'>
+            <div>
+                <div>
+                    <Board ></Board>
+                    <Board ></Board>
+                    <Board ></Board>
+                </div>
+                <div>
+                    <Board  ></Board>
+                    <Board ></Board>
+                    <Board ></Board>
+                </div>
+                <div>
+                    <Board ></Board>
+                    <Board ></Board>
+                    <Board ></Board>
+                </div>
+            </div>
+        </div>
+
+    </>
 }
